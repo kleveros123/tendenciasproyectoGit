7df75fa2925fcd8da9f966b29b9a76a91bfae616
klever <klever.os123@gmail.com>
2021-03-16 18:35:42 -0500

    Cambios al archivo acercade.php

--- acercade.php
+++ acercade.php
@@ -1,1 +1,16 @@
-Acerca de ....
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca de</title>
+</head>
+<body>
+    <h2>Acerca de la Empresa</h2>
+    <h3>Misión</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid laboriosam enim numquam porro facilis distinctio repudiandae pariatur animi. Cupiditate ipsam dolore laborum, neque maxime sapiente mollitia aspernatur tenetur possimus nam?</p>
+    <h3>Visión</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem atque in accusantium repellendus perspiciatis nam quod voluptatem delectus labore adipisci ad sequi facere provident, odit assumenda amet. In, consectetur dolore.</p>
+</body>
+</html>
